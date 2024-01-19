93726a97d5a7fd36dbd7fe316f26b34a4ff5f6fc
Willian Pinheiro <willpinhaaa@gmail.com>
2024-01-19 17:18:18 -0300

    add deploy to pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind()],
+  site: "https://oklch.willpinha.link"
 });
