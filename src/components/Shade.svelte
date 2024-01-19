afd95112343b3c41c76b05d6983d5d2c13ab1526
Willian Pinheiro <willpinhaaa@gmail.com>
2024-01-19 17:54:04 -0300

    remove Color component for optimization

--- src/components/Shade.svelte
+++ src/components/Shade.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-    import { chroma } from "../stores";
-
-    import Color from "./Color.svelte";
+    import { chroma, copied } from "../stores";
 
     export let hue: number;
 
@@ -19,6 +17,16 @@
         return lights.reverse();
     }
 
+    function copyCode(light: number) {
+        navigator.clipboard.writeText(`oklch(${light}% ${$chroma} ${hue})`);
+
+        $copied = true;
+
+        setTimeout(() => {
+            $copied = false
+        }, 1000);
+    }
+
     const lights = buildLights();
 </script>
 
@@ -26,8 +34,16 @@
     <span class="text-sm">{hue}H</span>
 
     <div class="flex gap-2">
-        {#each lights as l}
-            <Color light={l} chroma={$chroma} {hue} />
+        {#each lights as light}
+            <div class="flex flex-col items-center">
+                <button
+                    on:click={() => copyCode(light)}
+                    class="px-6 py-5 rounded shadow hover:shadow-lg hover:-translate-y-2 hover:duration-200 hover:cursor-copy"
+                    style="background-color: oklch({light}% {$chroma} {hue})">
+                </button>
+
+                <span class="text-sm">{light}L</span>
+            </div>
         {/each}
     </div>
 </div>
